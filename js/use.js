b6b16f4d5cfffbe3e9044ea547c6d5593df0faa2
Biihuy <biihuy25697@gmail.com>
2021-03-16 11:04:26 +0700

    a

--- js/use.js
+++ js/use.js
@@ -1,4 +1,14 @@
-const __loaded = $('.loaded');
+// const __loaded = $('.loaded');
+// $(window).on('load', function () {
+//     __loaded.remove();
+// });
 $(window).on('load', function () {
-    __loaded.remove();
-})
+    let _id = window.location.href.split('#')[1];
+    let ww = $(window).innerWidth();
+    //let aa = $('#'+_id).offset().top;
+    let hh = 140;
+    ww > 768 ? hh = $('#' + _id).offset().top - 140 : hh = $('#' + _id).offset().top - 80;
+    $('body, html').animate({
+        scrollTop: hh
+    }, 0);
+});
